617e2def958b618195bd093529056afe453f4f17
janik2102 <janik.siller@googlemail.com>
2018-06-07 22:50:40 +0200

    Picture Sending added

--- WatchBot.py
+++ WatchBot.py
@@ -1,18 +1,19 @@
 import telegram
 import json
+import os
+import datetime
 
 from picamera import PiCamera
 from time import sleep
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 #Read JSON Config with Bot Token and Admin-Identity
-with open('Configuration.json') as config_json:
-    configuration = json.load(config_json)
+configuration = json.load('configuration.json')
 
 token = configuration.get('token')
 admin = configuration.get('admin')
-videopath = configuration.get('vidpath')
-picturepath = configuration.get('picpath')
+vidpath = configuration.get('vidpath')
+picpath = configuration.get('picpath')
 temppath = configuration.get('temppath')
 systemon = True
 camera = PiCamera()
@@ -31,18 +32,40 @@ def off(bot, update):
 def picture(bot, update):
     if (isAdmin(update.message.chat.username)):
         update.message.reply_text('Picture of the room.')
+        tmpfile = makepicture()
+        update.message.reply_photo(photo=open(tmpfile, 'rb'))
+        deletefile(tmpfile)
+
 
 def video(bot, update):
     if (isAdmin(update.message.chat.username)):
         update.message.reply_text('Video of the room.')
-        camera.start_recording(temppath + '/tmpvid.h264')
-        sleep(10)
-        camera.stop_recording()
-        bot.sendVideo(update.message.chat_id, temppath + '/tmpvid.h264')
+
 
 #Define Methods
+#check if user is admin
 def isAdmin(identity):
     return admin.get('username') == identity
+
+#make video and return the filepath
+def makevideo():
+    currentTime = datetime.datetime.now()
+    path = vidpath + '/' + currentTime + '.h264'
+    camera.start_recording(path)
+    sleep(10)
+    camera.stop_recording()
+    return path
+
+#make picture and return the filepath
+def makepicture():
+    currentTime = datetime.datetime.now()
+    path = picpath + '/' + currentTime + 'jpg'
+    camera.capture(path)
+    return path
+
+#delete file in filesystem
+def deletefile(path):
+    os.remove(path)
 
 updater = Updater(token)
 updater.dispatcher.add_handler(CommandHandler('on', on))
